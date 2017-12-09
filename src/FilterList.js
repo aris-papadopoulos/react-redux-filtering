55624aa960f6dda8c4247763f3e0bd4b2f44df53
Aris Papadopoulos <aris.ppd@gmail.com>
2017-12-09 18:29:04 +0200

    Refactor filterlist

--- src/FilterList.js
+++ src/FilterList.js
@@ -3,18 +3,24 @@ import React, { Component } from 'react';
 
 export default class FilterList extends Component {
     
+    createFilters(){
+
+        return this.props.filters.map((el, index) =>
+            <div key={index}>
+                <input type="checkbox" key={index} name="filter" id={el} value={el} />
+                <label>{el}</label>
+            </div>
+        )
+
+    }
+    
+
     render() {
         return (
                <fieldset>
                     <legend>{this.props.name}</legend>
-                    {
-                        
-                        this.props.filters.map((el, index) =>
-                        <div key={index}>
-                            <input type="checkbox" key={index} name="filter" id={el} value={el} />
-                            <label>{el}</label>
-                        </div>
-                        )
+                    {   
+                        this.createFilters()
                     }
                 </fieldset>
         )
